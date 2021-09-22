617c79251e7483c58ae31df001411dfef87a1074
patrikduch <duchpatrik@icloud.com>
2021-09-22 21:10:11 +0200

    NET: Items microservice - structure setup

---
+++ src/services/Items.API/Items.API/Configurations/DatabaseConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace Items.API.Configurations
+{
+    public class DatabaseConfig
+    {
+        /// <summary>
+        /// Gets or sets connection to the database.
+        /// </summary>
+        public string ConnectionString { get; set; }
+    }
+}
